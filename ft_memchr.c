4aba0c4eb29d545813abdbff8449572031e378ae
Eric Cerqueira Leao <ericcleao@gmail.com>
2020-02-01 12:04:51 -0300

    Add "ft_memchr" exercise
    - It returns the pointer to the first 'c' appear
    
    Signed-off-by: Eric Cerqueira Leao <ericcleao@gmail.com>

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 23:05:31 by ecerquei          #+#    #+#             */
+/*   Updated: 2020/01/21 23:12:47 by ecerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	str = (unsigned char*)s;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 19:43:03 by ecerquei          #+#    #+#             */
-/*   Updated: 2020/02/01 11:57:26 by ecerquei         ###   ########.fr       */
+/*   Updated: 2020/02/01 12:04:39 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	ft_memchr(const void *s, int c, size_t n);
 
 int		ft_islower(int c);
 int		ft_isspace(int c);
