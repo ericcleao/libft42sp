8d1fee3d1179c13f5b05f7296fa7b105f4540954
Eric Cerqueira Leao <ericcleao@gmail.com>
2020-02-01 12:08:12 -0300

    Add "ft_memcmp" exercise
    - Its compare the two given pointers until "n"
    - It returns the difference between s1 and s2 at the different position
    
    Signed-off-by: Eric Cerqueira Leao <ericcleao@gmail.com>

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 19:43:03 by ecerquei          #+#    #+#             */
-/*   Updated: 2020/02/01 12:04:39 by ecerquei         ###   ########.fr       */
+/*   Updated: 2020/02/01 12:07:53 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 int		ft_islower(int c);
 int		ft_isspace(int c);
