37ac7e1a823aa3e8b43dd0d730536943aeba6230
Eric Cerqueira Leao <ericcleao@gmail.com>
2020-02-01 11:52:24 -0300

    Add "ft_isascii" exercise
    - Check if the passed character is between 0 and 127
    
    Signed-off-by: Eric Cerqueira Leao <ericcleao@gmail.com>

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,30 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/21 19:43:03 by ecerquei          #+#    #+#             */
-/*   Updated: 2020/02/01 11:53:52 by ecerquei         ###   ########.fr       */
+/*   Created: 2020/01/21 22:24:11 by ecerquei          #+#    #+#             */
+/*   Updated: 2020/01/24 22:19:48 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdlib.h>
-
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-
-int		ft_islower(int c);
-int		ft_isspace(int c);
-int		ft_isupper(int c);
-
-#endif
+int	ft_isascii(int c)
+{
+	return (c >= 0 && c <= 127);
+}
