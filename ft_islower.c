9c231d390112889d3f072bf5f6bddc8e5b9cd6e6
Eric Cerqueira Leao <ericcleao@gmail.com>
2020-02-01 11:44:31 -0300

    Add extra functions "ft_islower" and "ft_isupper"
    - Those function will be used on some obrigatory functions
    
    Signed-off-by: Eric Cerqueira Leao <ericcleao@gmail.com>

--- ft_islower.c
+++ ft_islower.c
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/21 19:43:03 by ecerquei          #+#    #+#             */
-/*   Updated: 2020/02/01 11:44:06 by ecerquei         ###   ########.fr       */
+/*   Created: 2020/01/21 21:09:37 by ecerquei          #+#    #+#             */
+/*   Updated: 2020/01/24 22:12:10 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdlib.h>
-
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-int		ft_isdigit(int c);
-
-int		ft_islower(int c);
-int		ft_isspace(int c);
-int		ft_isupper(int c);
-
-#endif
+int	ft_islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
