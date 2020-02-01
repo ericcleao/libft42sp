a27bf4227bf41b5bb333d76024e5430a197394af
Eric Cerqueira Leao <ericcleao@gmail.com>
2020-02-01 11:55:32 -0300

    Add "ft_isprint" exercise
    - Checks if the passed character is between ' ' and '~'
    - Copying the standard "isprint" function
    
    Signed-off-by: Eric Cerqueira Leao <ericcleao@gmail.com>

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,31 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/21 19:43:03 by ecerquei          #+#    #+#             */
-/*   Updated: 2020/02/01 11:53:52 by ecerquei         ###   ########.fr       */
+/*   Created: 2020/01/21 22:47:44 by ecerquei          #+#    #+#             */
+/*   Updated: 2020/01/21 23:04:12 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdlib.h>
-
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-
-int		ft_islower(int c);
-int		ft_isspace(int c);
-int		ft_isupper(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	return (c >= ' ' && c <= '~');
+}
