2b7b8ab97ebaf33240feefa6112a0b9f596505d2
Eric Cerqueira Leao <ericcleao@gmail.com>
2020-02-01 11:50:46 -0300

    Add "ft_isalnum" exercise
    - Checks if the passed character is an alpha character or a digit
    
    Signed-off-by: Eric Cerqueira Leao <ericcleao@gmail.com>

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_alnum.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/21 19:43:03 by ecerquei          #+#    #+#             */
-/*   Updated: 2020/02/01 11:50:09 by ecerquei         ###   ########.fr       */
+/*   Created: 2020/01/21 22:44:19 by ecerquei          #+#    #+#             */
+/*   Updated: 2020/01/21 22:46:37 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
-
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-
-int		ft_islower(int c);
-int		ft_isspace(int c);
-int		ft_isupper(int c);
-
-#endif
+int	ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
